f7615b130fa6203bfe0bebc6fefd8f0c00cc69a7
imomaikel <imomaikeldev@gmail.com>
2024-10-23 16:38:12 +0200

    feat: add footer

--- src/app/(dashboard)/_components/Footer.tsx
+++ src/app/(dashboard)/_components/Footer.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className='text-center p-1'>
+			<span className='text-sm text-right text-muted-foreground'>
+				Michał Kołodziejczyk built this web app as part of a Fullstack Developer
+				Recruitment Task for Code & Pepper 2024.
+			</span>
+		</div>
+	);
 };
 
 export default Footer;
