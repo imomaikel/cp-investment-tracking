6c70bca38e1aab1d4f866136d7cece57fe45c0dd
imomaikel <imomaikeldev@gmail.com>
2024-10-23 16:32:57 +0200

    feat: add dashboard layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,22 @@
+import Footer from './_components/Footer';
+import Navbar from './_components/Navbar';
+
 const DashboardPage = () => {
-	return <div>Dashboard</div>;
+	return (
+		<div className='min-h-screen flex flex-col'>
+			<nav>
+				<Navbar />
+			</nav>
+
+			<div className='max-w-screen-2xl mx-auto flex flex-1 flex-col w-full'>
+				<section className='flex flex-col flex-1'></section>
+
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		</div>
+	);
 };
 
 export default DashboardPage;
